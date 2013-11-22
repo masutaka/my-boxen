2226b3853cf8000d9f9e627b787b973cf286e7e3
Takashi Masuda <masutaka.net@gmail.com>
2013-11-23 03:59:29 +0900

    Add global version of node.js and ruby and perl

--- modules/people/manifests/masutaka.pp
+++ modules/people/manifests/masutaka.pp
@@ -24,7 +24,11 @@ class people::masutaka {
      ]:
   }
 
+  class { 'nodejs::global': version => 'v0.10' }
+
   $ruby_version = '2.0.0'
+
+  class { 'ruby::global': version => $ruby_version }
 
   ruby::gem { "ghn for ${ruby_version}":
     gem  => 'ghn',
@@ -37,6 +41,8 @@ class people::masutaka {
   }
 
   $perl_version = '5.18.1'
+
+  class { 'perl::global': version => $perl_version }
 
   perl::version { $perl_version: }
 
