6420f226e1573412e7989f40cab70b9a0c65cb19
Takashi Masuda <masutaka.net@gmail.com>
2013-11-23 00:59:44 +0900

    Add masutaka.pp

---
+++ modules/people/manifests/masutaka.pp
@@ -0,0 +1,27 @@
+class people::masutaka {
+
+  package {
+    [
+     'aspell',
+     'bazaar',
+     'binutils',
+     'cmigemo',
+     'global',
+     'gnu-sed',
+     'gnupg',
+     'imagemagick',
+     'jq',         # json pretty-print
+     'jsl',
+     'lv',
+     'markdown',
+     'mercurial',
+     'stunnel',    # mew
+     'unrar',
+     'w3m',
+     'webkit2png', # % webkit2png -TF http://masutaka.net
+     'wget',
+     'xz',
+     ]:
+  }
+
+}
